95275ae146f93215e5a7045c7e10862f93ef35af
daniel <daniel.stolzberg@gmail.com>
2020-06-23 14:36:08 -0400

    removed smoothing factor

--- fus_toNifti.m
+++ fus_toNifti.m
@@ -1,24 +1,16 @@
-function v = fus_toNifti(Plane,ffn,smoothFactor)
-% v = fus_toNifti(Plane,ffn,smoothFactor)
+function v = fus_toNifti(Plane,ffn)
+% v = fus_toNifti(Plane,ffn)
 % 
 % ffn           -  output filename
-% smoothFactor  -  number of pixes for within plane 2d convolution with
-%                  gaussian kernel (default = 5)
 
 if nargin < 2 || isempty(ffn),          ffn = 'fus_MeanVolume.nii'; end
-if nargin < 3 || isempty(smoothFactor), smoothFactor = 5; end
 
 fprintf('Creating nifti: "%s" ...',ffn)
 
 % create nifti mean volume
 v = cast([],'like',Plane(1).Data);
-gw = gausswin(smoothFactor);
 for i = 1:length(Plane)
     I = Plane(i).I;
-    X = sqrt(mean(Plane(i).Data.^2,[I.dFrames, I.dTrials, I.dStim]));
-    nv = max(abs(X(:)));
-    X = conv2(gw,gw',X,'same');
-    X = X ./ max(abs(X(:))) .* nv;
     v(:,:,i) = reshape(X,[I.nX I.nY]);
 end
 
