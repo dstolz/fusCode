4807facf7e7b522be64666395a177c9dde6355af
DJS <daniel.stolzberg@gmail.com>
2020-07-09 13:04:53 -0400

    added additional functionality and comments

--- SCRATCH_fUS_Object.m
+++ SCRATCH_fUS_Object.m
@@ -14,22 +14,47 @@ V = fus.Volume(epoch_bef,dims);
 
 disp(V)
 
-clear epoch_bef
+clear epoch_bef % don't need original data anymore
+
+
+%% View montage of "Structural"
+figure
+V.montage;
+
+
+%% Explore averaged trial data for each stimulus for a plane
+% This will update as plane data is manipulated if left open
+planeID = 1;
+V.Plane(planeID).explorer('Circle');
 
 
 
 %% Rigid alignment of all planes
 V.align_planes;
 
+
+%% Draw masks on each Plane
+for i = 1:V.nPlanes
+    V.Plane(i).Mask.create_roi;
+end
+
+
+
+%% Volumetric spatial smoothing
+N = [3 3 3];
+V.smooth(N);
+
+
 %% Apply Baseline correction to all planes in the volume
 % process_planes is a convenient function for running the same function on
 % all planes.
-
-tic
 baselineWindow = [0 8]; % seconds
 V.process_planes(@baseline_correct,baselineWindow);
-toc
 
-%%
 
-V.Plane(5).explorer;
+%% Use the Log associated with each plane to see how the Plane Data has been manipulated so far
+V.Plane(1).print_log
+
+
+
+
