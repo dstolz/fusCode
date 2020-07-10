e7ac9d3f200dd33e48110cd9bb2e89eb87724ff2
DJS <daniel.stolzberg@gmail.com>
2020-07-10 15:24:24 -0400

    added help comments

--- obj/helperFuncs/estimate_hr.m
+++ obj/helperFuncs/estimate_hr.m
@@ -1,6 +1,28 @@
 function hr = estimate_hr(obj,stdThresh)
 % hr = estimate_hr(Volume,[zThreshold])
 % hr = estimate_hr(Plane,[zThreshold]
+%
+% Estimate of the mean haemodynamic response function (Boubenec et al,
+% 2018):
+% Convert all within-mask samples of each plane to z-score, threshold for
+% peak responses greater than or equal to stdThresh standard deviations, 
+% compute the average of these responses.
+%
+% Note that the returned waveform is the entire timecourse which likely
+% includes pre-response fluctuations that should be removed prior to
+% convolution with experiment design.
+%
+%
+% Inputs:
+%   obj        ... either one fus.Volume or one or more fus.Plane objects
+%   stdThresh  ... standard deviation threshold (default = 3);
+%
+% Output:
+%   hr         ... mean estimate of the haemodynamic response.
+
+% DJS 2020
+
+
 
 if nargin < 3 || isempty(stdThresh), stdThresh = 3; end
 
