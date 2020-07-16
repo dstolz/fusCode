b8547e489679e11d389fc5134d9d6b022695ff66
DJS <daniel.stolzberg@gmail.com>
2020-07-16 10:42:13 -0400

    added example to import raw fUS data and add an Event

--- SCRATCH_fUS_Object.m
+++ SCRATCH_fUS_Object.m
@@ -2,19 +2,75 @@
 addpath(genpath('c:\src\fusCode'))
 
 
-%% Add planes to a new Volume object
-load('D:\fUS_Data\Boubenec\Data_Bright_AC\Boubenec_Data_Bright_AC.mat')
 
+
+%% Example 1: Add planes to a new Volume object
+load('D:\fUS_Data\Boubenec\Data_Bright_AC\Boubenec_Data_Bright_AC.mat')
 
 % dims are in the same order as that of the loaded data
 % Dims 'Y' and 'X' must come first
 dims = {'Y','X','Stim','Trials','Frames','Planes'};
 
-V = fus.Volume(epoch_bef,dims);
+V = fus.Volume(epoch_bef,dims); % create Volume
+
+clear epoch_bef % don't need original data anymore
 
 disp(V)
 
-clear epoch_bef % don't need original data anymore
+
+
+%% Example 2: Load raw data and stimulus Events into a Volume
+pth = 'D:\fUS_Data\Ali_RawData\rum069'; % Root directory
+
+dims = {'Y','X','Frames'}; % dims must start with 'Y','X',...
+
+% find all planes stored within the current directory
+d = dir(pth);
+d([1 2]) = [];
+d(~[d.isdir]) = [];
+
+
+
+
+stim = [602 1430 3400 8087 19234];
+load('D:\fUS_Data\Ali_RawData\rum069\info_Tonotopics_rum069.mat'); % info
+% manipulate info -> Plane x event
+stim = stim(info(1:2:end,:));
+trialOnsetIdx = info(2:2:end,:);
+stimDelay    = 10; % sec ??? not specified in file
+stimDuration = 3;  % sec ??? not specified in file
+
+
+clear E
+
+V = fus.Volume; % initialize
+for i = 1:length(d)
+    % load the acquisition file
+    lpth = fullfile(d(i).folder,d(i).name);
+    f = dir(fullfile(lpth,'*.acq'));
+    ffn = fullfile(f.folder,f.name);
+    load(ffn,'-mat') 
+    
+    Fs = 1./Acquisition.Param.dT_Doppler; % sampling rate
+    
+    data = squeeze(Acquisition.Data); % squeeze out singleton dimension in data
+    data = permute(data,[2 1 3]); % transpose each frame
+    
+    clear Acquisition % discard original data
+    
+    V.add_plane(data,dims,Fs);
+    
+
+    stimOnsetTime = (trialOnsetIdx(i,:)-1)./Fs+stimDelay;
+    V.Plane(i).Event = fus.Event('Freq',stimOnsetTime,stim(i,:),stimDuration,Fs,'Hz');
+end
+
+[~,n] = fileparts(pth);
+V.Name = n;
+
+disp(V)
+
+
 
 
 %% View montage of "Structural"
@@ -25,7 +81,7 @@ V.montage;
 %% Explore averaged trial data for each stimulus for a plane
 % This will update as plane data is manipulated if left open
 planeID = 4;
-V.Plane(planeID).explorer('Rectangle');
+V.Plane(planeID).explorer; % defaults to 'Rectangle'
 % V.Plane(planeID).explorer('Circle');
 % V.Plane(planeID).explorer('Polygon');
 
