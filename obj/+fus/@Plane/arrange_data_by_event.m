3b0e290e044132b9509e027122e2be14fcf10ea9
DJS <daniel.stolzberg@gmail.com>
2020-07-18 11:50:48 -0400

    fixed the fix

--- obj/+fus/@Plane/arrange_data_by_event.m
+++ obj/+fus/@Plane/arrange_data_by_event.m
@@ -77,7 +77,7 @@ for i = 1:length(S)
     d = nan(npx,nsmps,S(i).N,'like',data);
     idx(idx<1|idx>size(data,2)) = nan;
     for j = 1:S(i).N
-        d(:,~isnan(idx(j,:)),j) = data(:,~isnan(idx(j,:)));
+        d(:,~isnan(idx(j,:)),j) = data(:,idx(j,:));
     end
     A(i).Data    = d;
     A(i).Samples = idx;
