6ad578c72d526801219a7a7fbf1eba9e9dddf7df
DJS <daniel.stolzberg@gmail.com>
2020-07-19 10:54:24 -0400

    Added comments

--- obj/planeFuncs/plane_subset.m
+++ obj/planeFuncs/plane_subset.m
@@ -1,12 +1,22 @@
-function M = plane_subset(M,ind)
-% M = plane_subset(M,ind)
+function R = plane_subset(A,ind)
+% R = plane_subset(A,ind)
+% 
+% Returns a subset of n-dimensional matrix A using the 2D logical matrix,
+% ind.  Dims of ind must be equal to the first two dims of A, but A can
+% have any number of additional dimensions.
+%
+% The return matrix, R, has ndims(A)-1 dimensions, with the first dimension
+% containing all indices from the first two dimensions of A.
+% 
 
-n = size(M);
+% DJS 2020
+
+n = size(A);
 
 assert(all(size(ind) == n(1:2)), 'plane_subset:DimensionMismatch', ...
     'size(ind) must equal [size(M,1) size(M,2)]')
 
   
-M = reshape(M,[prod(n(1:2)) prod(n(3:end))]);
-M = M(ind(:),:);
-M = reshape(M,[nnz(ind) 1 n(3:end)]);
+A = reshape(A,[prod(n(1:2)) prod(n(3:end))]);
+A = A(ind(:),:);
+R = reshape(A,[nnz(ind) 1 n(3:end)]);
