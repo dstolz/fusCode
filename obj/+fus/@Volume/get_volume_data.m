804eaff12b40cd2e66ea2f1780149cd07aac9d0c
DJS <daniel.stolzberg@gmail.com>
2020-08-12 14:54:25 -0400

    minor

--- obj/+fus/@Volume/get_volume_data.m
+++ obj/+fus/@Volume/get_volume_data.m
@@ -1,8 +1,9 @@
 function v = get_volume_data(obj)
 
 v = [];
+n = obj.nDims;
 for i = 1:obj.nPlanes
-    v = cat(obj.nDims,v,obj.Plane(i).Data);
+    v = cat(n,v,obj.Plane(i).Data);
 end
 
 v = permute(v,[1 2 obj.nDims 3:obj.Plane(1).nDims]);
